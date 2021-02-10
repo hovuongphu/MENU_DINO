1fe84a9f3ded59d53be355ae8168afe777d8fd65
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-02-10 11:00:10 -0800

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -18,11 +18,22 @@ local avatar = Instance.new("ImageLabel")
 local ImageLabel = Instance.new("ImageLabel")
 local blogin = Instance.new("ImageLabel")
 local run = Instance.new("TextButton")
+local menuset = Instance.new("ImageLabel")
+local openset = Instance.new("TextButton")
 local loading = Instance.new("Frame")
 local Frame = Instance.new("ImageLabel")
 local namegame = Instance.new("TextLabel")
 local ID_2 = Instance.new("ImageLabel")
 local IDgame = Instance.new("TextLabel")
+local menuset_2 = Instance.new("Frame")
+local textby = Instance.new("ImageLabel")
+local TextLabel = Instance.new("TextLabel")
+local keylogin = Instance.new("ImageLabel")
+local key = Instance.new("TextBox")
+local backr = Instance.new("ImageLabel")
+local TextButton = Instance.new("TextButton")
+local entkey = Instance.new("ImageLabel")
+local entkeyr = Instance.new("TextButton")
 
 --Properties:
 
@@ -34,7 +45,7 @@ bia.Parent = newmenu
 bia.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 bia.BackgroundTransparency = 1.000
 bia.Position = UDim2.new(0.340398192, 0, 0.135876045, 0)
-bia.Size = UDim2.new(0, 497, 0, 611)
+bia.Size = UDim2.new(0, 497, 0, 689)
 bia.Image = "rbxassetid://3570695787"
 bia.ImageColor3 = Color3.fromRGB(53, 53, 53)
 bia.ScaleType = Enum.ScaleType.Slice
@@ -45,8 +56,8 @@ menu.Name = "menu"
 menu.Parent = bia
 menu.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 menu.BackgroundTransparency = 1.000
-menu.Position = UDim2.new(0.020120725, 0, 0.0196399353, 0)
-menu.Size = UDim2.new(0, 476, 0, 586)
+menu.Position = UDim2.new(0.020120725, 0, 0.0196070559, 0)
+menu.Size = UDim2.new(0, 476, 0, 660)
 menu.Image = "rbxassetid://3570695787"
 menu.ImageColor3 = Color3.fromRGB(61, 61, 61)
 menu.ScaleType = Enum.ScaleType.Slice
@@ -161,7 +172,7 @@ blogin.Name = "blogin"
 blogin.Parent = login
 blogin.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 blogin.BackgroundTransparency = 1.000
-blogin.Position = UDim2.new(0.800000012, 0, 4.86000013, 0)
+blogin.Position = UDim2.new(0.810000002, 0, 4.89000034, 0)
 blogin.Size = UDim2.new(0, 314, 0, 73)
 blogin.Image = "rbxassetid://3570695787"
 blogin.ImageColor3 = Color3.fromRGB(91, 91, 91)
@@ -173,6 +184,7 @@ run.Name = "run"
 run.Parent = blogin
 run.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 run.BackgroundTransparency = 1.000
+run.Position = UDim2.new(0.00318471342, 0, 0, 0)
 run.Size = UDim2.new(0, 314, 0, 73)
 run.Font = Enum.Font.Roboto
 run.Text = "GO"
@@ -180,6 +192,30 @@ run.TextColor3 = Color3.fromRGB(0, 0, 0)
 run.TextScaled = true
 run.TextSize = 14.000
 run.TextWrapped = true
+
+menuset.Name = "menuset"
+menuset.Parent = login
+menuset.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+menuset.BackgroundTransparency = 1.000
+menuset.Position = UDim2.new(0.810000002, 0, 5.9000001, 0)
+menuset.Size = UDim2.new(0, 314, 0, 44)
+menuset.Image = "rbxassetid://3570695787"
+menuset.ImageColor3 = Color3.fromRGB(91, 91, 91)
+menuset.ScaleType = Enum.ScaleType.Slice
+menuset.SliceCenter = Rect.new(100, 100, 100, 100)
+menuset.SliceScale = 0.120
+
+openset.Name = "openset"
+openset.Parent = menuset
+openset.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+openset.BackgroundTransparency = 1.000
+openset.Size = UDim2.new(0, 312, 0, 44)
+openset.Font = Enum.Font.SourceSans
+openset.Text = "Setting"
+openset.TextColor3 = Color3.fromRGB(0, 0, 0)
+openset.TextScaled = true
+openset.TextSize = 14.000
+openset.TextWrapped = true
 
 loading.Name = "loading"
 loading.Parent = menu
@@ -236,9 +272,112 @@ IDgame.TextScaled = true
 IDgame.TextSize = 14.000
 IDgame.TextWrapped = true
 
+menuset_2.Name = "menuset"
+menuset_2.Parent = menu
+menuset_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+menuset_2.BackgroundTransparency = 1.000
+menuset_2.Size = UDim2.new(0, 100, 0, 100)
+menuset_2.Visible = false
+
+textby.Name = "textby"
+textby.Parent = menuset_2
+textby.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+textby.BackgroundTransparency = 1.000
+textby.Position = UDim2.new(0.930000007, 0, 0.280000001, 0)
+textby.Size = UDim2.new(0, 287, 0, 60)
+textby.Image = "rbxassetid://3570695787"
+textby.ImageColor3 = Color3.fromRGB(91, 91, 91)
+textby.ScaleType = Enum.ScaleType.Slice
+textby.SliceCenter = Rect.new(100, 100, 100, 100)
+textby.SliceScale = 0.120
+
+TextLabel.Parent = textby
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Size = UDim2.new(0, 287, 0, 60)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "BY DINO#5485"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+keylogin.Name = "keylogin"
+keylogin.Parent = menuset_2
+keylogin.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+keylogin.BackgroundTransparency = 1.000
+keylogin.Position = UDim2.new(0.629999995, 0, 1.61000001, 0)
+keylogin.Size = UDim2.new(0, 346, 0, 60)
+keylogin.Image = "rbxassetid://3570695787"
+keylogin.ImageColor3 = Color3.fromRGB(91, 91, 91)
+keylogin.ScaleType = Enum.ScaleType.Slice
+keylogin.SliceCenter = Rect.new(100, 100, 100, 100)
+keylogin.SliceScale = 0.120
+
+key.Name = "key"
+key.Parent = keylogin
+key.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+key.BackgroundTransparency = 1.000
+key.Position = UDim2.new(-0.000463232136, 0, 0, 0)
+key.Size = UDim2.new(0, 346, 0, 60)
+key.Font = Enum.Font.SourceSans
+key.PlaceholderText = "KEY HERE"
+key.Text = ""
+key.TextColor3 = Color3.fromRGB(0, 0, 0)
+key.TextScaled = true
+key.TextSize = 14.000
+key.TextWrapped = true
+
+backr.Name = "backr"
+backr.Parent = menuset_2
+backr.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+backr.BackgroundTransparency = 1.000
+backr.Position = UDim2.new(0.949999988, 0, 5.57000017, 0)
+backr.Size = UDim2.new(0, 287, 0, 60)
+backr.Image = "rbxassetid://3570695787"
+backr.ImageColor3 = Color3.fromRGB(91, 91, 91)
+backr.ScaleType = Enum.ScaleType.Slice
+backr.SliceCenter = Rect.new(100, 100, 100, 100)
+backr.SliceScale = 0.120
+
+TextButton.Parent = backr
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.Size = UDim2.new(0, 285, 0, 60)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "BACK"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+entkey.Name = "entkey"
+entkey.Parent = menuset_2
+entkey.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+entkey.BackgroundTransparency = 1.000
+entkey.Position = UDim2.new(0.920000017, 0, 2.28999996, 0)
+entkey.Size = UDim2.new(0, 287, 0, 31)
+entkey.Image = "rbxassetid://3570695787"
+entkey.ImageColor3 = Color3.fromRGB(91, 91, 91)
+entkey.ScaleType = Enum.ScaleType.Slice
+entkey.SliceCenter = Rect.new(100, 100, 100, 100)
+entkey.SliceScale = 0.120
+
+entkeyr.Name = "entkeyr"
+entkeyr.Parent = entkey
+entkeyr.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+entkeyr.BackgroundTransparency = 1.000
+entkeyr.Size = UDim2.new(0, 285, 0, 31)
+entkeyr.Font = Enum.Font.SourceSans
+entkeyr.Text = "GET"
+entkeyr.TextColor3 = Color3.fromRGB(0, 0, 0)
+entkeyr.TextScaled = true
+entkeyr.TextSize = 14.000
+entkeyr.TextWrapped = true
+
 -- Scripts:
 
-local function TYUUDUI_fake_script() -- ImageLabel.LocalScript 
+local function VNSEKW_fake_script() -- ImageLabel.LocalScript 
 	local script = Instance.new('LocalScript', ImageLabel)
 
 	local Players = game:GetService("Players")
@@ -256,8 +395,8 @@ local function TYUUDUI_fake_script() -- ImageLabel.LocalScript
 	imageLabel.Image = content
 	
 end
-coroutine.wrap(TYUUDUI_fake_script)()
-local function ECUOZ_fake_script() -- newmenu.LocalScript 
+coroutine.wrap(VNSEKW_fake_script)()
+local function RUOGNKS_fake_script() -- newmenu.LocalScript 
 	local script = Instance.new('LocalScript', newmenu)
 
 	--ERRO
@@ -273,6 +412,10 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 		game.Players.LocalPlayer:Kick("Erro#003")
 	end
 	
+	function erro004()
+		game.Players.LocalPlayer:Kick("Erro#004")
+	end
+	----
 	function erro00A()
 		game.Players.LocalPlayer:Kick("You are banned/Erro#00A")
 	end
@@ -285,6 +428,9 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 		game.Players.LocalPlayer:Kick("Erro#00C")
 	end
 	
+	function erro00D()
+		game.Players.LocalPlayer:Kick("Erro#00D")
+	end
 	
 	---menu
 	local bia = script.Parent.bia
@@ -294,17 +440,28 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 	local menuloding = script.Parent.bia.menu.loading
 	local User = script.Parent.bia.menu.login.nameID.bname.name
 	local ID = script.Parent.bia.menu.login.nameID.bID.ID
-	local rank = script.Parent.bia.menu.login.nameID.brank.rank
+	local Userrank = script.Parent.bia.menu.login.nameID.brank.rank
 	local IDgame = script.Parent.bia.menu.loading.ID.IDgame
 	local namegame = script.Parent.bia.menu.loading.Frame.namegame
 	--login
 	local run = script.Parent.bia.menu.login.blogin.run
+	
+	
+	
+	--set
+	local menuset = script.Parent.bia.menu.menuset
+	local openset = script.Parent.bia.menu.login.menuset.openset
+	local keyset = script.Parent.bia.menu.menuset.keylogin.key
+	local backset = script.Parent.bia.menu.menuset.backr.TextButton
+	local getkey = script.Parent.bia.menu.menuset.entkey.entkeyr
 	
 	--ID show
 	ID.Text = game.Players.LocalPlayer.userId
 	User.Text = game.Players.LocalPlayer.Name
 	IDgame.Text = game.GameId
 	
+	--free
+	Userrank.Text = "FREE"
 	
 	--tapro run game
 	function runall()
@@ -337,11 +494,14 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 	
 	
 	--run ahck
+	function rundatascripFRRE()
+		roghoul()
+		waitr()
+	end
+	
 	function rundatascrip()
 		roghoul()
 	end
-	
-	
 	--hack
 	function roghoul()
 		if IDgame.Text == "380704901" then
@@ -352,7 +512,32 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 			bia.Visible = false
 			runall()
 			loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/hovuongphu/MENU_DINO/main/Ro-Ghoul.lau")))()
-		end	
+			if IDgame.Text == "380704901" then
+			end	
+		else
+	
+			erro00D()
+		end
+	end
+	
+	--open setting
+	
+	openset.MouseButton1Click:Connect(function()
+		menulogin.Visible = false
+		menuset.Visible = true
+	end)
+	
+	backset.MouseButton1Click:Connect(function()
+		menulogin.Visible = true
+		menuset.Visible = false
+	end)
+	
+	--free-reming
+	
+	
+	function waitr()
+		wait(1800)
+		erro004()
 	end
 	
 	
@@ -362,29 +547,20 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 	
 	
 	
+	getkey.MouseButton1Click:Connect(function()
+		if User.Text == "hovuoghu4446" or User.Text == "anhbecun1611" and keyset.Text == "0001" or User.Text == "minhkhang2007" and keyset.Text == "0002" or User.Text == "DeathDemonvt" and keyset.Text == "0003" or User.Text == "kubin197763" and keyset.Text == "0004" or User.Text == "ruakhung1181" and keyset.Text == "0005" or User.Text == "khoahatenoob2" and keyset.Text == "0006" then
+			Userrank.Text = "Premium"
+			menuset.Visible = false
+			wait()
+			menuloding.Visible = true
+			rundatascripFRRE()
+			if not passuser() then
+			end	
+		else
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+			erro001()
+		end
+	end)
 	
 	
 	
@@ -443,24 +619,73 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 	function passuser()
 		
 	end
-	function DATAUSER()
-		if User.Text == "hovuoghu4446" and ID.Text == "552402824" or User.Text == "anhbecun1611" and ID.Text == "364527443" or User.Text == "minhkhang2007" and ID.Text == "164781558" or User.Text == "DeathDemonvt" and ID.Text == "1518537802" or User.Text == "kubin197763" and ID.Text == "658332594" or User.Text == "ruakhung1181" and ID.Text == "129049230" or User.Text == "khoahatenoob2" and ID.Text == "1090933762" then
-			rank.Text = "Premium"
+	
+	
+	
+	function DATAADMIN()
+		if User.Text == "hovuoghu4446" and ID.Text == "552402824" then
+			Userrank.Text = "ADMIN"
+	
+		end
+	end
+	
+	DATAADMIN()
+	
+	run.MouseButton1Click:Connect(function()
+		if User.Text == "hovuoghu4446" then
+			menulogin.Visible = false
+			wait()
+			menuloding.Visible = true
+			rundatascrip()
 			if not passuser() then
 			end	
 		else
-			
-			erro001()
-		end
-	end
-	DATAUSER()
 	
-	run.MouseButton1Click:Connect(function()
-		menulogin.Visible = false
-		wait()
-		menuloding.Visible = true
-		rundatascrip()
+			menulogin.Visible = false
+			wait()
+			menuloding.Visible = true
+			rundatascripFRRE()
+		end
 	end)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	--ban
 	function banscr()
 		loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/hovuongphu/MENU_DINO/main/Ban-user.lua")))()
@@ -554,7 +779,5 @@ local function ECUOZ_fake_script() -- newmenu.LocalScript
 	
 	
 	
-	
-	
 end
-coroutine.wrap(ECUOZ_fake_script)()
+coroutine.wrap(RUOGNKS_fake_script)()
