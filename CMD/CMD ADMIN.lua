f87e4c1a876d01baa247ec74be59fa415af5b7f9
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-02-24 20:10:27 +0700

    Update CMD ADMIN.lua

--- CMD/CMD ADMIN.lua
+++ CMD/CMD ADMIN.lua
@@ -2,9 +2,9 @@ function update()
 	game:GetService("StarterGui"):SetCore("SendNotification", {
 		Title = "system!!";
 		Text = "HEY UPDATE!";
-		Duration = 1;
+		Duration = 10;
 	})
-	wait(0)
+	wait(10)
 	game.Players.LocalPlayer:Kick("Please wait for the new update")
 end
-update()
+--update()
