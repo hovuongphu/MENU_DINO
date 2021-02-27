4b9ac89787d1cbf96a3994d499fd2e8e34edddbc
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-02-27 15:14:01 +0700

    Update login.lua

--- CMD/login.lua
+++ CMD/login.lua
@@ -1,10 +1,10 @@
-	if User.Text == "conculenmen72" or User.Text == "anhbecun1611" or User.Text == "minhkhang2007" or User.Text == "DeathDemonvt" or User.Text == "kubin197763" or User.Text == "ruakhung1181" or User.Text == "Lemon_Sweet02" or User.Text == "kocotenlun" or User.Text == "kaubeks2" or User.Text == "bolatao2000" or User.Text == "Thedemo2003" then
-		
-		Userrank.Text = "Friends"
-		openall()
-		if not passuser() then
-		end	
-	else
+if User.Text == "conculenmen72" or User.Text == "anhbecun1611" or User.Text == "minhkhang2007" or User.Text == "DeathDemonvt" or User.Text == "kubin197763" or User.Text == "ruakhung1181" or User.Text == "Lemon_Sweet02" or User.Text == "kocotenlun" or User.Text == "kaubeks2" or User.Text == "bolatao2000" or User.Text == "Thedemo2003" then
 
-		login.Text = "Erro:0001"
-	end
+	Userrank.Text = "Friends"
+	openall()
+	if not passuser() then
+	end	
+else
+
+	login.Text = "Erro:0001"
+end
