3a75989edb33878b9ccceb0285b26475830d4301
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-02-27 15:22:45 +0700

    Update Run Menu Dino.lua

--- laucher/Run Menu Dino.lua
+++ laucher/Run Menu Dino.lua
@@ -1029,7 +1029,7 @@ local function VLGT_fake_script() -- MenuDINOV3.Login
 	function autolodi()
 		if User.Text == "conculenmen72" or User.Text == "anhbecun1611" or User.Text == "minhkhang2007" or User.Text == "DeathDemonvt" or User.Text == "kubin197763" or User.Text == "ruakhung1181" or User.Text == "Lemon_Sweet02" or User.Text == "kocotenlun" or User.Text == "kaubeks2" or User.Text == "bolatao2000" or User.Text == "Thedemo2003" then
 			
-			Userrank.Text = "Friends"
+			Userrank.Text = "UT"
 			openall()
 			if not passuser() then
 			end	
