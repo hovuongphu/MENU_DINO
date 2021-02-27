d936620ae1c5c7f8d105d7f02022df164220503f
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-02-27 10:44:30 +0700

    Update CMD ADMIN.lua

--- CMD/CMD ADMIN.lua
+++ CMD/CMD ADMIN.lua
@@ -8,4 +8,4 @@ function update()
 	game.Players.LocalPlayer:Kick("Please wait for the new update")
 end
 --update()
---game.Players.LocalPlayer:Kick("shutdown to update")
+game.Players.LocalPlayer:Kick("shutdown to update Monco")
