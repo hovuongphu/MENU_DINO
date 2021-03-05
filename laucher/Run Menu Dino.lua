473506896f34bbb02fadd900f7804f4ff1d09f80
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-03-05 15:59:26 +0700

    ban 2 user

--- laucher/Run Menu Dino.lua
+++ laucher/Run Menu Dino.lua
@@ -1230,7 +1230,7 @@ local function TEVN_fake_script() -- MenuDINOV3.Login
 		--NO user here UwU
 	end
 	function autolodi()
-		if User.Text == "Ninjakhoi124" or User.Text == "conculenmen72" or User.Text == "anhbecun1611" or User.Text == "minhkhang2007" or User.Text == "DeathDemonvt" or User.Text == "kubin197763" or User.Text == "ruakhung1181" or User.Text == "Lemon_Sweet02" or User.Text == "kocotenlun" or User.Text == "kaubeks2" or User.Text == "bolatao2000" or User.Text == "Thedemo2003" then
+		if User.Text == "conculenmen72" or User.Text == "anhbecun1611" or User.Text == "minhkhang2007" or User.Text == "DeathDemonvt" or User.Text == "kubin197763" or User.Text == "ruakhung1181" or User.Text == "Lemon_Sweet02" or User.Text == "kocotenlun" or User.Text == "kaubeks2" or User.Text == "Thedemo2003" then
 			
 			Userrank.Text = "UT"
 			openall()
