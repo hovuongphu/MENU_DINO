f00720b23327ef783e9c8c72386a9d3b613ab052
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-03-09 16:18:37 +0700

    a

--- System/Cmd System.lua
+++ System/Cmd System.lua
@@ -8,4 +8,4 @@ function update()
 	game.Players.LocalPlayer:Kick("Please wait for the new update")
 end
 --update()
-game.Players.LocalPlayer:Kick("shutdown for new update")
+--game.Players.LocalPlayer:Kick("shutdown for new update")
