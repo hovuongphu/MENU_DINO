e51c275a03a596ac37b7a1a770292f225d267faa
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-03-20 11:00:39 +0700

    a

--- Script game/Blox Fruits.lua
+++ Script game/Blox Fruits.lua
@@ -1,2 +1,1 @@
---loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnySalf/Hentai/main/MHEEHUB",true))()
-game.Players.LocalPlayer:Kick("Error#006")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BunnySalf/Hentai/main/MHEEHUB",true))()
