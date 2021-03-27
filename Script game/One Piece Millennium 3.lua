ee65b1d55eeb4e484ea32d39708ac6d0151d185a
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-03-27 20:01:27 +0700

    a

--- Script game/One Piece Millennium 3.lua
+++ Script game/One Piece Millennium 3.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/10x00/Public-Scripts/main/One%20Piece%3A%20Millennium%203"))()
+-- SETTING THEME --
+_G.MainI = Color3.fromRGB(203 ,156, 255)
+_G.MainII = Color3.fromRGB(140, 166 ,252)
+_G.ButtonI = Color3.fromRGB(203 ,156, 255)
+_G.ButtonII = Color3.fromRGB(140, 166 ,252)
+_G.BlackGround = Color3.fromRGB(140, 166 ,252)
+-- SCRIPT GAME --
+loadstring(game:HttpGet("http://skyhubking.xyz/script/free_script/main%20game.lua"))()
