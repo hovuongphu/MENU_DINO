205ed1824a7809397a7233cc4bcf3e6637724275
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-03-28 21:40:09 +0700

    a

--- Controle/System.lua
+++ Controle/System.lua
@@ -1,9 +1,10 @@
 local controle = 1
 --Update
 while controle < 2 do
-	wait(5)
     --Update
-	loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/hovuongphu/Dino-Hud/main/System/Cmd%20System.lua")))()
+	loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/hovuongphu/Dino-Hud/main/Controle/File/Update.lua")))()
     --Ban
+    loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/hovuongphu/Dino-Hud/main/Controle/File/BanUser.lua")))()
+    wait(5)
 end
 
