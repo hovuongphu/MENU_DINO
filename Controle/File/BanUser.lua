c0a280fdc9f3f5545208d46691262bf13c98cfc8
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-03-28 21:37:53 +0700

    a

---
+++ Controle/File/BanUser.lua
@@ -0,0 +1,5 @@
+local pls = game.Players.LocalPlayer
+
+if pls.Name == "Hou" then
+    game.Players.LocalPlayer:Kick("You got banned from Dino Hud")		
+end
--- Controle/System.lua
+++ Controle/System.lua
@@ -1,30 +1,9 @@
 local controle = 1
-
-
-function Ban()
-	if pls.Name == "No1" then
-        game.Players.LocalPlayer:Kick("You got banned from Dino Hud")		
-	end
-end
-
-function Update()
-	game.Players.LocalPlayer:Kick("Dino hud server stopped working due to maintenance and Update")
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
+--Update
 while controle < 2 do
-    Ban()
-    Update()
-    wait(5)
+	wait(5)
+    --Update
+	loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/hovuongphu/Dino-Hud/main/System/Cmd%20System.lua")))()
+    --Ban
 end
+
