6c54a6f0f7ed0af27b6b8416c928de682ab551ce
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-04-04 13:11:32 +0700

    a

--- System/Login User.lua
+++ System/Login User.lua
@@ -6,15 +6,17 @@ Check.Visible = false
 
 local Time = {
 	---Premium
-	["kaubeks2"] = {Year = 2021, Month = 4, Day = 17},
-	["DeathDemonvt"] = {Year = 2021, Month = 4, Day = 14},
-	["Tatminh307"] = {Year = 2021, Month = 4, Day = 14},
-	["bolatao2000"] = {Year = 2021, Month = 6, Day = 1},
+	["kaubeks2"] = {Year = 2021, Month = 5, Day = 2},
+	["DeathDemonvt"] = {Year = 2021, Month = 4, Day = 29},
+	["Tatminh307"] = {Year = 2021, Month = 4, Day = 29},
 
-	["sdsdsd"] = {Year = 2021, Month = 4, Day = 5},
+	["anhbecun1611"] = {Year = 2021, Month = 4, Day = 19},
 
 
-	
+	["sdsdsd"] = {Year = 2021, Month = 4, Day = 20},
+
+	---Freeday
+	["bolatao2000"] = {Year = 2021, Month = 4, Day = 1},
 
 
 }
@@ -35,7 +37,8 @@ function Premium()
 			Check.Text = "2"
 		else
 			Check.Text = "1"
-			rconsoleinfo("Rank:Premium | Expired:4/17")
+			rconsoleinfo("Event April")
+			rconsoleinfo("Rank:Premium | Expired:5/2")
 			rconsolewarn("Thanks for joining (Dino-Hud)")
 			rconsoleprint(" " .. " \n")
 		end
@@ -47,7 +50,8 @@ function Premium()
 			Check.Text = "2"
 		else
 			Check.Text = "1"
-			rconsoleinfo("Rank:Premium | Expired:4/14")
+			rconsoleinfo("Event April")
+			rconsoleinfo("Rank:Premium | Expired:4/29")
 			rconsolewarn("Thanks for joining (Dino-Hud)")
 			rconsoleprint(" " .. " \n")
 		end
@@ -59,7 +63,8 @@ function Premium()
 			Check.Text = "2"
 		else
 			Check.Text = "1"
-			rconsoleinfo("Rank:Premium | Expired:4/14")
+			rconsoleinfo("Event April")
+			rconsoleinfo("Rank:Premium | Expired:4/29")
 			rconsolewarn("Thanks for joining (Dino-Hud)")
 			rconsoleprint(" " .. " \n")
 		end
@@ -71,41 +76,30 @@ function Premium()
 			Check.Text = "2"
 		else
 			Check.Text = "1"
-			rconsoleinfo("Rank:Premium | Expired:4/5")
+			rconsoleinfo("Event April")
+			rconsoleinfo("Rank:Premium | Expired:4/20")
 			rconsolewarn("Thanks for joining (Dino-Hud)")
 			rconsoleprint(" " .. " \n")
 		end
 	end
 	--//////////////
-	if User.Name == "bolatao2000" then
-		local expire = Time.bolatao2000
+	if User.Name == "anhbecun1611" then
+		local expire = Time.anhbecun1611
 		if date.year >= expire.Year and date.month >= expire.Month and date.day >= expire.Day then
 			Check.Text = "2"
 		else
 			Check.Text = "1"
-			rconsoleinfo("Rank:Premium | Expired:6/1")
+			rconsoleinfo("Event April")
+			rconsoleinfo("Rank:Premium | Expired:4/19")
 			rconsolewarn("Thanks for joining (Dino-Hud)")
 			rconsoleprint(" " .. " \n")
 		end
 	end
-end
 
-Premium()
-
-
-
-
-
-
-
-
-
-function Freeday()
 	
 end
 
-
-
+Premium()
 
 
 
@@ -141,13 +135,13 @@ end
 
 function Ban()
 	--//////////////
-	if User.Name == "anhbecun1611" then
+	if User.Name == "NOO" then
 		local expire = BanTime.anhbecun1611
 		if date.year >= expire.Year then
 			Check.Text = "3"
 		else
 			Check.Text = "3"
-			rconsoleinfo("Rank:Ban | Expired:999999999Day")
+			rconsoleinfo("Rank:Ban | Expired:Forever")
 			rconsolewarn("Thanks for joining (Dino-Hud)")
 			rconsoleprint(" " .. " \n")
 		end
@@ -221,8 +215,32 @@ end
 
 
 
-
-
+function Freeday()
+	--//////////////
+	if User.Name == "bolatao2000" then
+		local expire = Time.bolatao2000
+		if date.year >= expire.Year and date.month >= expire.Month and date.day >= expire.Day then
+			Check.Text = "2"
+		else
+			Check.Text = "1"
+			rconsoleinfo("Rank:Premium | Expired:4/1")
+			rconsolewarn("Thanks for joining (Dino-Hud)")
+			rconsoleprint(" " .. " \n")
+		end
+	end
+	--//////////////
+	if User.Name == "anhbecun1611" then
+		local expire = Time.anhbecun1611
+		if date.year >= expire.Year and date.month >= expire.Month and date.day >= expire.Day then
+			Check.Text = "2"
+		else
+			Check.Text = "1"
+			rconsoleinfo("Rank:Free Try | Expired:5/2")
+			rconsolewarn("Thanks for joining (Dino-Hud)")
+			rconsoleprint(" " .. " \n")
+		end
+	end
+end
 Freeday()
 
 
