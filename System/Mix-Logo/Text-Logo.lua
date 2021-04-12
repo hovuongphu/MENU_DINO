e33e984ae2368c681ef0a8a7cf67b7c329f5d9bd
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-04-12 19:59:56 +0700

    a

--- System/Mix-Logo/Text-Logo.lua
+++ System/Mix-Logo/Text-Logo.lua
@@ -7,14 +7,8 @@ TextLabel.BackgroundTransparency = 1.000
 TextLabel.Position = UDim2.new(0.00606796145, 0, 0.952324212, 0)
 TextLabel.Size = UDim2.new(0, 127, 0, 31)
 TextLabel.Font = Enum.Font.Gotham
-TextLabel.Text = ""
+TextLabel.Text = "Dino Hud"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
-local function WUKL_fake_script()
-	local script = Instance.new('LocalScript', TextLabel)
-
-	script.Parent.Text = "Dino Hud"
-end
-coroutine.wrap(WUKL_fake_script)()
