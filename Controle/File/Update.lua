913edd176f79ecff1f3ab4f7db952cf6c085c96d
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-04-17 13:24:06 +0700

    a

--- Controle/File/Update.lua
+++ Controle/File/Update.lua
@@ -1,5 +1,2 @@
-function Update()
-    game.Players.LocalPlayer:Kick("stopped working!")
-    rconsolewarn("Dino hud server stopped working due to maintenance and Update")
-end
-Update()
+game.Players.LocalPlayer:Kick("stopped working!")
+rconsolewarn("Dino hud server stopped working due to maintenance and Update")
