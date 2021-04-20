2e60bee5c32e4ff1b1f4d50f7946b012ac9a1f0e
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-04-21 04:14:49 +0700

    a

--- Controle/File/Update.lua
+++ Controle/File/Update.lua
@@ -3,3 +3,8 @@ game.Players.LocalPlayer:kick'UwU'
 rconsolewarn("Dino hud server stopped working for maintenance or update or Fix")
 ]]
 --
+local pls = game.Players.LocalPlayer
+
+if pls.Name == "chuotti2008" then
+    game.Players.LocalPlayer:Kick("You was given to the blacklist!")		
+end
