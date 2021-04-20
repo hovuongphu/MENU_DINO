4dbf6de567b471154eb5dbab12995a4563235c19
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-04-20 22:44:28 +0700

    a

--- Script game/Blade Quest.lua
+++ Script game/Blade Quest.lua
@@ -1,35 +1,36 @@
 -- Dungeon Settings
 
-_G.endlessmode = false -- Will only run endless mode and will reset dungeon after highest level gear is obtained (or the last area is cleared if lastendlesszone is not false), need to be lvl 25+
+_G.endlessmode = true -- Will only run endless mode and will reset dungeon after highest level gear is obtained (or the last area is cleared if lastendlesszone is not false), need to be lvl 25+
 
 _G.lastendlesszone = false -- leave false if farming for items and/or not farming xp, otherwise set to a number which will be the last area you will do before you start a new endless run
 
 _G.endlessmodelimittest = false -- DO NOT LEAVE ON WHILE AFKING ENDLESS MODE, only meant for pushing the limits of the autofarm in endless mode (for leaderboards and shit)
 
-_G.hardestdungeonpossible = false -- Will ignore custom dungeon settings below (except for hardcore) and play highest dungeon possible
+_G.hardestdungeonpossible = true -- Will ignore custom dungeon settings below (except for hardcore) and play highest dungeon possible
 
-_G.Hardcore = false -- Should be fine for the most part to keep on, but if for some reason you keep dying while autofarming, set to false temporarily until you no longer die.
+_G.Hardcore = true -- Should be fine for the most part to keep on, but if for some reason you keep dying while autofarming, set to false temporarily until you no longer die.
 
 -- Custom Dungeon Settings
 
-
-_G.Privatelobby = false -- Stops randoms from joining your lobby, keep on to avoid suspision and reports
+_G.Location = "Crystal Mines" -- Locations include Dragon's Den, Toy World, Crystal Mines, Ghost Town, and Forest
+_G.Difficulty = "Expert" -- Difficulties include Easy, Medium, Hard, and Expert
+_G.Privatelobby = true -- Stops randoms from joining your lobby, keep on to avoid suspision and reports
 
 -- Loot Settings
 
-_G.AutoSell = false -- Automatically sells gear if it is worse than your current gear (Based on damage and level)
+_G.AutoSell = true -- Automatically sells gear if it is worse than your current gear (Based on damage and level)
 
-_G.KeepLegendaries = false -- Will not sell legendaries if you obtain one
+_G.KeepLegendaries = true -- Will not sell legendaries if you obtain one
 
-_G.KeepMythics = false -- will not sell mythics if you obtain one
+_G.KeepMythics = true -- will not sell mythics if you obtain one
 
-_G.AutoUpgradeSkill = false -- Automatically puts level up points into damage to increase efficiency of autofarm
+_G.AutoUpgradeSkill = true -- Automatically puts level up points into damage to increase efficiency of autofarm
 
-_G.BuySwords = false -- will buy a rare+ sword of your level if you have the gold for it
+_G.BuySwords = true -- will buy a rare+ sword of your level if you have the gold for it
 
-_G.UpgradeEpics = false -- will keep any epics earned and will merge them into legendaries when possible, not worth keeping on unless you want to collect every legendary
+_G.UpgradeEpics = true -- will keep any epics earned and will merge them into legendaries when possible, not worth keeping on unless you want to collect every legendary
 
-_G.UpgradeLegendaries = false -- will keep any legendaries earned and will merge them into mythics when possible, best used super late game (lvl 60+)
+_G.UpgradeLegendaries = true -- will keep any legendaries earned and will merge them into mythics when possible, best used super late game (lvl 60+)
 
 -- Misc Settings
 
