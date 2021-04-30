e17621747c3db194fa596d7b49b4d738814b9043
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-04-30 21:31:35 +0700

    a

--- System/Login User.lua
+++ System/Login User.lua
@@ -11,6 +11,8 @@ local Time = {
 	["Tatminh307"] = {Year = 2021, Month = 4, Day = 29},
 	["chuotti2008"] = {Year = 2021, Month = 5, Day = 12},
 	["kocotenlun"] = {Year = 2021, Month = 5, Day = 30},
+	------
+	["FreeDay"] = {Year = 2021, Month = 5, Day = 3},
 }
 
 local BanTime = {
@@ -79,6 +81,18 @@ function Premium()
 			Check.Text = "1"
 			rconsoleinfo("ID Server:" .. (math.random(100000000, 999999999)))
 			rconsoleinfo("Rank:Premium | Expired:5/30")
+			rconsolewarn("Thanks for joining (Dino-Hud)")
+		end
+	end
+	--//////////////Free
+	if User.Name == "dirtdiamonh" or User.Name == "minhkhang2007" then
+		local expire = Time.kocotenlun
+		if date.year >= expire.Year and date.month >= expire.Month and date.day >= expire.Day then
+			Check.Text = "2"
+		else
+			Check.Text = "1"
+			rconsoleinfo("ID Server:" .. (math.random(100000000, 999999999)))
+			rconsoleinfo("Rank:Free3Day | Expired:5/3")
 			rconsolewarn("Thanks for joining (Dino-Hud)")
 		end
 	end
