6512f83ae0e9f73350d4a7799237639f10f509f1
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-05-02 01:10:03 +0700

    a

--- Controle/File/Update.lua
+++ Controle/File/Update.lua
@@ -1,7 +1,2 @@
---[[
-game.Players.LocalPlayer:kick'UwU'
-rconsolewarn("Dino hud server stopped working for Big Update Soon")
-]]
---
 game.Players.LocalPlayer:kick'UwU'
 rconsolewarn("Dino hud server stopped working for Update Server|sorry about this")
