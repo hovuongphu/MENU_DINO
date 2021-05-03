6b8b3d42a26adfd15913836306b21a65aa79631b
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-05-03 23:13:39 +0700

    a

--- Controle/File/Update.lua
+++ Controle/File/Update.lua
@@ -3,5 +3,5 @@ game.Players.LocalPlayer:kick'UwU'
 rconsolewarn("Dino hud server stopped working for Update Server|sorry about this")]]
 
 if User.Name == "chuotti2008" then
-    game.Players.LocalPlayer:kick'UwU'
+    game.Players.LocalPlayer:kick'Error Data'
 end
