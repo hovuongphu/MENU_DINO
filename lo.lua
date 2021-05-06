823ba945af8f185c46f5dcf33c3ba4bc5544d304
SPGREENDALAT <62071429+hovuongphu@users.noreply.github.com>
2021-05-07 00:40:13 +0700

    a

--- lo.lua
+++ lo.lua
@@ -1,19 +1,24 @@
 local isfolder = isfolder or false
 if (isfolder == false) then return game.Players.LocalPlayer:Kick("Exploit not supported! Missing: isfolder.") end
 --
-rconsolewarn("DinoHud | Setting up configuration settings")
+--rconsolewarn("DinoHud | Setting up configuration settings")
 --FILE DINOHUD
 if not isfolder("DinoHud_Server") then
-	rconsoleinfo("creating DinoHud folder")
+	--rconsoleinfo("creating DinoHud folder")
 	makefolder("DinoHud_Server")
 end
 --FILE DATA
 if not isfolder("DinoHud_Server/Data") then
-	rconsoleinfo("creating DinoHud folder")
+	--rconsoleinfo("creating DinoHud folder")
 	makefolder("DinoHud_Server/Data")
 end
 --Game
 if not isfolder("DinoHud_Server/Data/Game") then
-	rconsoleinfo("creating DinoHud folder")
+	--rconsoleinfo("creating DinoHud folder")
 	makefolder("DinoHud_Server/Data/Game")
+end
+--UserData
+if not isfolder("DinoHud_Server/UserData") then
+	--rconsoleinfo("creating DinoHud folder")
+	makefolder("DinoHud_Server/UserData")
 end
